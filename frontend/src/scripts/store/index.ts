4828f2c45717d782cab4f50c0cc31fb751ee0d18
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2020-10-21 18:10:43 +0200

    Update dependencies

--- frontend/src/scripts/store/index.ts
+++ frontend/src/scripts/store/index.ts
@@ -8,7 +8,6 @@ import routes from 'scripts/store/routes';
 import router from 'diox/extensions/router';
 
 const store = new Store();
-console.log(router([]));
 store.register('router', router(Object.keys(routes)));
 
 export { Store, store };
