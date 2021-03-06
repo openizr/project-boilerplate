031b50e7343532f75afd474639ab1052e92e77c6
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2021-03-06 17:01:56 +0100

    Fix catch-all content parser

--- backend/src/scripts/main.ts
+++ backend/src/scripts/main.ts
@@ -40,9 +40,7 @@ if (configuration.mode === 'development') {
 // Catch-all for unsupported content types. Prevents fastify from throwing HTTP 500 when dealing
 // with unknown payloads. See https://www.fastify.io/docs/latest/ContentTypeParser/.
 app.addContentTypeParser('*', (_request, payload, next) => {
-  let data = '';
-  payload.on('data', (chunk) => { data += chunk; });
-  payload.on('end', () => { next(null, data); });
+  next(null, payload);
 });
 
 // Adding app routes...
