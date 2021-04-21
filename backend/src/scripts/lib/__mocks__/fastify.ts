65e1116bd3168455f3baf2bd8ada7206296eede1
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2021-04-21 19:29:25 +0200

    Update dependencies

--- backend/src/scripts/lib/__mocks__/fastify.ts
+++ backend/src/scripts/lib/__mocks__/fastify.ts
@@ -6,13 +6,15 @@ const addHook = jest.fn((_event, callback) => callback({
 }, {
   header: jest.fn(),
   status: jest.fn(() => ({ send: jest.fn() })),
-}, null, jest.fn()));
+}, jest.fn()));
 
 const register = jest.fn((callback) => callback({
   post: jest.fn(),
   get: jest.fn(),
   head: jest.fn(),
 }, null, jest.fn()));
+
+const setValidatorCompiler = jest.fn((callback) => callback({ schema: {} }));
 
 const listen = jest.fn((_a, _b, callback) => callback(
   (process.env.ENV === 'production')
@@ -25,7 +27,8 @@ const fastify = jest.fn(() => ({
   register,
   get: jest.fn(),
   listen,
-  log: { fatal: jest.fn() },
+  setValidatorCompiler,
+  log: { fatal: jest.fn(), error: jest.fn() },
   setErrorHandler: jest.fn(),
   setNotFoundHandler: jest.fn(),
   addContentTypeParser: jest.fn(),
