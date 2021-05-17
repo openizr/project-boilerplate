fb3c7b848af8ddb7d9746ca56caca471a39a3466
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2021-05-17 17:21:48 +0200

    Improve base validation schema

--- backend/src/scripts/routes/v1/getMessage.ts
+++ backend/src/scripts/routes/v1/getMessage.ts
@@ -1,6 +1,5 @@
-import { deepMerge } from 'basx';
-import schema from 'scripts/lib/baseSchema';
 import { FastifyRequest, FastifyReply } from 'fastify';
+import createSchema from 'scripts/helpers/createSchema';
 
 /**
  * `GET /v1/message` endpoint handler.
@@ -9,16 +8,14 @@ export default {
   handler: (_request: FastifyRequest, response: FastifyReply): void => {
     response.send({ message: 'WELCOME_MESSAGE' });
   },
-  schema: deepMerge(schema,
-    {
-      body: undefined,
-      response: {
-        200: {
-          type: 'object',
-          properties: {
-            message: { type: 'string' },
-          },
+  schema: createSchema({
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
         },
       },
-    }),
+    },
+  }),
 };
--- backend/src/scripts/routes/v1/postMessage.ts
+++ backend/src/scripts/routes/v1/postMessage.ts
@@ -1,6 +1,5 @@
-import { deepMerge } from 'basx';
-import schema from 'scripts/lib/baseSchema';
 import { FastifyRequest, FastifyReply } from 'fastify';
+import createSchema from 'scripts/helpers/createSchema';
 
 /**
  * `POST /v1/message` endpoint handler.
@@ -9,29 +8,28 @@ export default {
   handler: (_request: FastifyRequest, response: FastifyReply): void => {
     response.send();
   },
-  schema: deepMerge(schema,
-    {
-      body: {
-        type: 'object',
-        required: ['message'],
-        properties: {
-          message: {
-            type: 'string',
-          },
-        },
-        errorMessage: {
-          properties: {
-            message: 'Body.message must be a string',
-          },
-          required: {
-            message: 'Body.message is required',
-          },
-        },
-      },
-      response: {
-        200: {
+  schema: createSchema({
+    body: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: {
           type: 'string',
         },
       },
-    }),
+      errorMessage: {
+        properties: {
+          message: 'Body.message must be a string',
+        },
+        required: {
+          message: 'Body.message is required',
+        },
+      },
+    },
+    response: {
+      200: {
+        type: 'string',
+      },
+    },
+  }),
 };
