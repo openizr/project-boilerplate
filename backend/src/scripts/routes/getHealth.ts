cbbe36874bc14d87a852867b9d38666e8f91bfa9
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2021-05-23 11:31:56 +0200

    Improve default routes

--- backend/src/scripts/routes/getHealth.ts
+++ backend/src/scripts/routes/getHealth.ts
@@ -5,10 +5,9 @@ import createSchema from 'scripts/helpers/createSchema';
  * `GET /health` endpoint handler.
  */
 export default {
-  handler: (_request: FastifyRequest, response: FastifyReply): Promise<void> => Promise.resolve()
-    .then(() => {
-      response.send();
-    }),
+  handler: (_request: FastifyRequest, response: FastifyReply): void => {
+    response.send();
+  },
   schema: createSchema({
     response: {
       200: {
