d43e12875e02538920df46ef39ee260b2bf8e65d
Matt <matthieu.jabbour@gmail.com>
2021-07-17 16:42:55 +0200

    Update NPM dependencies, fix formData parser

--- backend/src/scripts/__mocks__/fs-extra.ts
+++ backend/src/scripts/__mocks__/fs-extra.ts
@@ -3,7 +3,22 @@
  */
 const writeFile = jest.fn();
 const remove = jest.fn();
-const createWriteStream = jest.fn(() => ({ write: jest.fn() }));
+const createWriteStream = jest.fn(() => ({
+  end: jest.fn(),
+  write: jest.fn(),
+  on: jest.fn((eventName, callback) => {
+    if (eventName === 'close') {
+      callback();
+    }
+    setTimeout(() => {
+      if (process.env.FS_ERROR_STREAM === 'true') {
+        callback(new Error('error'));
+      } else {
+        callback();
+      }
+    }, 100);
+  }),
+}));
 const existsSync = jest.fn((path) => (path === '/var/www/html/node_modules/.cache/test'));
 const readFile = jest.fn((_path, callback) => {
   if (process.env.FS_ERROR === 'true') {
