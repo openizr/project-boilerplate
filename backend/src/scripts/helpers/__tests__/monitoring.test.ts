b1545dffbeb2e76ef4598b77d09dd04a1f49d1ca
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2022-03-14 12:40:42 +0100

    Fix type issues

--- backend/src/scripts/helpers/__tests__/monitoring.test.ts
+++ backend/src/scripts/helpers/__tests__/monitoring.test.ts
@@ -1,7 +1,9 @@
+/* eslint-disable global-require */
+
 describe('helpers/formatError', () => {
   jest.spyOn(process, 'exit').mockImplementation((code: number | undefined) => code as unknown as never);
   Object.assign(console, { error: jest.fn() });
-  process.on = jest.fn((_event, callback) => callback({ message: 'fatal error', stack: 'stack' }));
+  process.on = jest.fn((_event, callback) => callback({ message: 'fatal error', stack: 'stack' })) as Any;
   const { error } = console;
 
   beforeEach(() => {
